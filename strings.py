a7336ff52238a9414016152f0bf6cbc04833f0a7
adenosinetp10 <adenosinetp10@gmail.com>
2022-09-09 08:25:32 +0100

    changed github repo link

--- strings.py
+++ strings.py
@@ -18,7 +18,7 @@ ME_MSG = """
 <b>Total Questions :</b> <code>{}</code>
 """
 
-GITHUB_URL = "https://github.com/notPlasticCat"
+GITHUB_URL = "https://github.com/adenosinetp10"
 DEV_URL = "https://t.me/f22raptor_uwu"
 
 AKI_LANG_CODE = {
